c1b27dd89a3f60ee88ac02f20f22ca02ccef713c
benchimnonso <bensonjnrr@gmail.com>
2023-04-10 18:33:30 -0700

    all

--- 0x09-static_libraries/2-strlen.c
+++ 0x09-static_libraries/2-strlen.c
@@ -2,19 +2,16 @@
 
 /**
  * _strlen - Returns the length of a string.
- * @s: string
+ * @s: string.
  *
- * Return: Length
+ * Return: Length.
  */
 int _strlen(char *s)
 {
-	int longi = 0;
+	int len = 0;
 
-	while (*s != '\0')
-	{
-		longi++;
-		s++;
-	}
+	while (*s++)
+		len++;
 
-	return (longi);
+	return (len);
 }
